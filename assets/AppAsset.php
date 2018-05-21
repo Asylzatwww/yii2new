40b66b63015db6934bd1e3d32427697b59fab9bb
admin <asylzatwww@yandex.ru>
2018-05-21 18:52:25 +0100

    Revert "folder name from web to public_html without code change"
    This reverts commit a49f233806ff95de72ff4e529649b242197e45e3.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,7 +18,7 @@ use yii\web\AssetBundle;
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
-    public $baseUrl = '@public_html';
+    public $baseUrl = '@web';
     public $css = [
         'css/site.css',
     ];
